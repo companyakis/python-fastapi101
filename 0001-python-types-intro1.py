1a126ec7f89728f473923b3f1e80c8524dc5cdeb
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-11 15:00:02 +0300

    Create 0001-python-types-intro1.py

---
+++ 0001-python-types-intro1.py
@@ -0,0 +1,7 @@
+def give_info(first_name: str, birth_year: int):
+    age = 2024 - birth_year
+    print(f"{first_name} is {age} years old.")
+
+give_info("Aygün", 1990) # Aygün is 34 years old.
+
+give_info("Bilge", 1978) # Bilge is 46 years old.
